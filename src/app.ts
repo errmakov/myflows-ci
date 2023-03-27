8626c2afa0b9de8fe60dac9fe73b9ad8fb0096d4
Denis Ermakov <denis@errmakov.com>
2023-03-27 11:17:39 +0200

    tg bot

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { execSync } from 'child_process';
 import crypto from 'crypto';
 import express, { NextFunction, Request, Response } from 'express';
 import config from './config/config.js';
 import { TConfigKey } from './types/types.js';
+
 
 let confKey = (process.env.SETTING || 'development') as TConfigKey
 const cfg = config[confKey];
@@ -44,30 +46,43 @@ app.use(express.json());
 // Apply middleware to all routes
 app.use(verifySignatureMiddleware);
 
+const tgpost = (text: string) => {
+    axios.post(`https://api.telegram.org/bot'${process.env.TG_BOT_TOKEN}'/sendMessage`, {
+                chat_id: process.env.TG_CHAT_ID,
+                text: `Deployment started!`,
+            });   
+}
+
 app.post('/githubhook/push', (req, res) => {
     try {
         if (req.body.ref.includes('stage')) {
             res.status(200).send('Deployment started!');
+            tgpost(`Deployment started: ${req.body.head_commit.id} by ${req.body.pusher.name}`);            
             console.log(`Push event received at ${new Date().toISOString()}`);
+            tgpost('run git checkout!');
             execSync(`cd ${process.env.DEPLOY_DIR} && git checkout ${process.env.TARGET_BRANCH} && git pull`);
+            tgpost('run npm run build!');
             execSync(`cd ${process.env.DEPLOY_DIR} && npm run build`);
+            
+            tgpost('run cypress e2n: npm run cy:run');
             execSync(`cd ${process.env.DEPLOY_DIR} && npm run cy:run:login`);
+            
+            
             execSync(`cd ${process.env.DEPLOY_DIR} && echo '${req.body.head_commit.url}///${req.body.head_commit.id}///${req.body.head_commit.timestamp}///${req.body.pusher.name}'> public/version.txt`);
-            // axios.post(`https://api.telegram.org/bot'${process.env.TG_API_KEY}'/sendMessage`, {
-            //     chat_id: process.env.TG_CHAT_ID,
-            //     text: 'Deployment successful!',
-            // });    
+            
+            tgpost(`Deployment finished: ${req.body.head_commit.id} by ${req.body.pusher.name}`);
+            
         } else {
             res.status(200).send(`Branch ${req.body.ref} is not allowed to deploy!`);
+            tgpost(`Branch ${req.body.ref} is not allowed to deploy!`);
         }
     } catch (e) {
-        console.log(`Deployment failed at ${new Date().toISOString()} \n with error: ${e}`);
+        console.log(`Deployment  ${req.body.head_commit.id} by ${req.body.pusher.name} failed at ${new Date().toISOString()} \n with error: ${e}`);
+        tgpost(`Deployment  ${req.body.head_commit.id} by ${req.body.pusher.name} failed at ${new Date().toISOString()} \n with error: ${e}`);
+        
+        tgpost(`Trying to rollback: git reset --hard HEAD@{1}`);
+        
         execSync('git reset --hard HEAD@{1}');
-
-        // axios.post(`https://api.telegram.org/bot'${process.env.TG_API_KEY}'/sendMessage`, {
-        //     chat_id: process.env.TG_CHAT_ID,
-        //     text: 'Deployment failed!',
-        // });
 
     }
 });
