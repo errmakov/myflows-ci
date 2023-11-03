0b81f3de1599647529f4c2f34925603200f43816
Denis Ermakov <denis@errmakov.com>
2023-11-03 12:14:06 +0100

    markdown

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: process.env.APP_NAME,
-      script: "npm run markdown",
+      script: "npm run start",
       restart_delay: 4000,
       watch: ["./dist"],
       ignore_watch: ["./node_modules", "*.js.map"],
